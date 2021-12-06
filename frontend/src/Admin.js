24d8ef440079e3463b1890e90fd350b88afacbb8
floroudier <floroudier@gmail.com>
2021-12-06 10:56:20 +0100

    Final commit

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import './Components/Widget.css';
+import './Admin.css';
 import {Link} from "react-router-dom";
 
 function Admin (props) {
@@ -7,6 +8,22 @@ function Admin (props) {
       <div>
         <Link to="/">App</Link>
         Interface pour modifier notre widget meeting
+        <div id="Add">
+          Ajoutez des rendez-vous à votre dashboard
+          <form>
+            <input type="text" placeholder="Date"/>
+            <input type="text" placeholder="Lieu"/>
+            <input type="submit" id="btn" value="Add" />
+          </form>
+        </div>
+        <div id="delete">
+          Supprimez des rendez-vous de votre dashboard
+          <form>
+            <input type="text" placeholder="Date"/>
+            <input type="text" placeholder="Lieu"/>
+            <input type="submit" id="btn" value="Delete" />
+          </form>
+        </div>
       </div>
     )
   }
